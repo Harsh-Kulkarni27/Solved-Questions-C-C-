3a87851654e595dc77a6b5443b074b7af89e7e24
Harsh-Kulkarni27 <112061252+Harsh-Kulkarni27@users.noreply.github.com>
2022-08-27 17:04:25 +0530

    Add files via upload

---
+++ AdditionStrings.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    string s1, s2, result;
+    cout<<"Enter 1st Part: "<<endl;
+    getline(cin,s1);
+
+    cout<<"Enter 2nd Part: "<<endl;
+    getline(cin,s2);
+
+    result= s1 + s2;
+
+    cout<<"Resultant String: "<< result;
+    
+
+
+
+
+
+    return 0;
+}
